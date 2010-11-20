b6dd0a540b77077a266f43cf18c5f09ac5c71bb0
Erik Johnson <CowboyCoderForever@users.noreply.github.com>
2010-11-20 01:15:37 +0000

    Updated to sharedCode r1521

--- ext/include/DeltaNetworkAdapter/MachineInfoList.h
+++ ext/include/DeltaNetworkAdapter/MachineInfoList.h
@@ -2,6 +2,10 @@
 #define MACHINE_INFO_LIST__H
 
 ////////////////////////////////////////////////////////////////////////////////
+#include <dtCore/refptr.h>
+#include <dtGame/machineinfo.h>
+
+#include <vector>
 
 typedef std::vector< dtCore::RefPtr<dtGame::MachineInfo> > MachineInfoList;
 
